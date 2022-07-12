376c559ed9a0641146948dfbd05d923c0546a38f
Yehor Dreval <freeksons@gmail.com>
2022-07-13 00:07:39 +0200

    test

--- src/App.js
+++ src/App.js
@@ -14,9 +14,9 @@ function App() {
             <h3>All pizzas</h3>
             <section className="pizza">
                 <PizzaItem title="Cheeseburger pizza" price={19} img="1" />
-                <PizzaItem title="Asian shrimp" price={22} />
-                <PizzaItem title="Cheese chicken" price={24} />
-                <PizzaItem title="Cheese" price={18} />
+                <PizzaItem title="Asian shrimp" price={22} img="2" />
+                <PizzaItem title="Cheese chicken" price={24} img="3" />
+                <PizzaItem title="Cheese" price={18} img="4" />
                 <PizzaItem title="Cheeseburger pizza" price={19} />
                 <PizzaItem title="Asian shrimp" price={22} />
                 <PizzaItem title="Cheese chicken" price={24} />
