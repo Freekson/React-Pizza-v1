35631a6f7ef93ae7a25a6ec5c0776b85b3e91efe
Yehor Dreval <freeksons@gmail.com>
2022-07-15 17:18:00 +0200

    render pizza and add logic for category

--- src/App.js
+++ src/App.js
@@ -5,10 +5,8 @@ import NavList from './components/NavList';
 import PizzaItem from './components/PizzaItem';
 
 import './scss/main.scss';
-import pizza1 from './assets/img/pizza-1.png';
-import pizza2 from './assets/img/pizza-2.png';
-import pizza3 from './assets/img/pizza-3.png';
-import pizza4 from './assets/img/pizza-4.png';
+
+import pizzas from './assets/json/pizzas.json';
 
 function App() {
     return (
@@ -17,14 +15,15 @@ function App() {
             <NavList />
             <h3>All pizzas</h3>
             <section className="pizza">
-                <PizzaItem title="Cheeseburger pizza" price={19} img={pizza1} />
-                <PizzaItem title="Asian shrimp" price={22} img={pizza2} />
-                <PizzaItem title="Cheese chicken" price={24} img={pizza3} />
-                <PizzaItem title="Cheese" price={18} img={pizza4} />
-                <PizzaItem title="Cheeseburger pizza" price={19} img={pizza1} />
-                <PizzaItem title="Asian shrimp" price={22} img={pizza2} />
-                <PizzaItem title="Cheese chicken" price={24} img={pizza3} />
-                <PizzaItem title="Cheese" price={18} img={pizza4} />
+                {pizzas.map((obj) => (
+                    <PizzaItem
+                        title={obj.title}
+                        price={obj.price}
+                        img={obj.imageUrl}
+                        sizes={obj.sizes}
+                        types={obj.types}
+                    />
+                ))}
             </section>
         </div>
     );
--- src/components/PizzaItem.jsx
+++ src/components/PizzaItem.jsx
@@ -1,26 +1,42 @@
 import React from 'react';
 
-function PizzaItem({ title, price, img }) {
-    const [pizzaCount, setPizzaCount] = React.useState(1);
-    const onClickButton = () => setPizzaCount(pizzaCount + 1);
+function PizzaItem({ title, price, img, sizes, types }) {
+    const typeNames = ['thin', 'traditional'];
+    const [activeType, setActiveType] = React.useState(0);
+    const [activeSize, setActiveSize] = React.useState(0);
+    const onClickType = (i) => {
+        setActiveType(i);
+    };
+    const onClickSize = (i) => {
+        setActiveSize(i);
+    };
     return (
         <div className="pizza__item">
-            <img src={img} alt="pizza" className="pizza__img" />
+            <img src={img} alt={title} className="pizza__img" />
             <p className="pizza__header">{title}</p>
             <div className="kind-section">
                 <div className="kind">
-                    <div className="kind-active">thin</div>
-                    <div>traditional</div>
+                    {types.map((typeId, index) => (
+                        <div
+                            onClick={() => onClickType(index)}
+                            className={activeType == index ? 'kind__active' : ''}>
+                            {typeNames[typeId]}
+                        </div>
+                    ))}
                 </div>
                 <div className="size">
-                    <div>26 cm.</div>
-                    <div className="size-active">30 cm.</div>
-                    <div>40 cm.</div>
+                    {sizes.map((sizeId, index) => (
+                        <div
+                            onClick={() => onClickSize(index)}
+                            className={activeSize == index ? 'kind__active' : ''}>
+                            {sizeId} cm.
+                        </div>
+                    ))}
                 </div>
             </div>
             <div className="price-section">
                 <p className="pizza-price">from {price} $</p>
-                <button className="btn active-btn" onClick={onClickButton}>
+                <button className="btn active-btn">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         width="12"
@@ -33,7 +49,7 @@ function PizzaItem({ title, price, img }) {
                         />
                     </svg>
                     <p>Add</p>
-                    <p className="amount">{pizzaCount}</p>
+                    <p className="amount">1</p>
                 </button>
             </div>
         </div>
